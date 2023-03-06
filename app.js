641b27dacae8c692220965beaa0e339900efbc02
suganthsugi <suganthjayanthi@gmail.com>
2023-03-06 23:43:47 +0530

    h

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors())
 
 const mongoose=require('mongoose');
 mongoose.connect("mongodb+srv://admin:admin@cluster0.zk2cgod.mongodb.net/?retryWrites=true&w=majority");
